2ded6fdff77631455e5807a05e6993f73c1598bc
KIMHOONHEE <nmdkims@gmail.com>
2022-04-28 08:44:35 +0900

    추가 - 리액트 장고 연동 예제 완료 #2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
+import {
+  HashRouter as Router,
+  Route
+} from "react-router-dom";
+
+
 import './App.css';
-import Header from "./components/Header";
-import NotesListPage from "./pages/NotesListPage";
+import Header from './components/Header'
+import NotesListPage from './pages/NotesListPage'
+import NotePage from './pages/NotePage'
 
 function App() {
-    return (
-        <div className="App">
-            <Header />
-            <NotesListPage />
+  return (
+    <Router>
+      <div className="container dark">
+        <div className="app">
+          <Header />
+          <Route path="/" exact component={NotesListPage} />
+          <Route path="/note/:id" component={NotePage} />
         </div>
-    );
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- src/pages/NotesListPage.js
+++ src/pages/NotesListPage.js
@@ -1,4 +1,7 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
+import ListItem from '../components/ListItem'
+import AddButton from '../components/AddButton'
+
 
 const NotesListPage = () => {
 
@@ -11,18 +14,24 @@ const NotesListPage = () => {
 
     let getNotes = async () => {
 
-        let response = await fetch('http://127.0.0.1:8000/api/notes/')
+        let response = await fetch('/api/notes/')
         let data = await response.json()
         setNotes(data)
     }
 
     return (
-        <div>
+        <div className="notes">
+            <div className="notes-header">
+                <h2 className="notes-title">&#9782; Notes</h2>
+                <p className="notes-count">{notes.length}</p>
+            </div>
+
             <div className="notes-list">
-                {notes.map((note, index)=>(
-                    <h3 key={index}>{note.body}</h3>
+                {notes.map((note, index) => (
+                    <ListItem key={index} note={note} />
                 ))}
             </div>
+            <AddButton />
         </div>
     )
 }
