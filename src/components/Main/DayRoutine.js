2de4ee159ab827cdabf2c8c90941b1df6d43d1ed
jungjaechoi <joong8812@gmail.com>
2022-05-12 23:23:02 +0900

    Feat: 추가 - 일과 추가에 따라 하루일정표에 동적으로 적용되도록 #7

--- src/components/Main/DayRoutine.js
+++ src/components/Main/DayRoutine.js
@@ -1,91 +1,33 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { useSelector } from "react-redux";
+
+import { getSeriesData, getRoutineTableOptions } from "./MyRoutineHelper";
 
 import { PieChart } from "@toast-ui/react-chart";
 
 import "./DayRoutine.css";
 import Modal from "../UI/Modal";
-import DayRoutineMaker from './DayRoutineMaker'
-
-let data = {
-  categories: ["MyRoutine"],
-  series: [
-    {
-      name: "수면",
-      data: 8,
-    },
-    {
-      name: "오전 공부",
-      data: 4,
-    },
-    {
-      name: "점심",
-      data: 1.5,
-    },
-    {
-      name: "오후 공부",
-      data: 4,
-    },
-    {
-      name: "저녁",
-      data: 1.5,
-    },
-    {
-      name: "운동",
-      data: 1,
-    },
-    {
-      name: "밤 공부",
-      data: 4,
-    },
-  ],
-};
-
-const options = {
-  chart: {
-    width: 500,
-    height: 500,
-  },
-  series: {
-    dataLabels: {
-      visible: true,
-      pieSeriesName: {
-        visible: true,
-      },
-      formatter: (data) => {
-        const percentage = +data.split("%")[0];
-        let hour = Number((24 * (percentage / 100)).toFixed(1));
-        if (hour - hour.toFixed(0) === 0) hour = hour.toFixed(0);
-        return `${hour} Hours`;
-      },
-    },
-    angleRange: {
-      start: -15,
-      end: 345,
-    },
-    // selectable: true
-  },
-  exportMenu: {
-    visible: false,
-  },
-  legend: {
-    visible: false,
-  },
-  theme: {
-    series: {
-      hover: {
-        lineWidth: 1,
-      },
-    },
-  },
-};
+import DayRoutineMaker from "./DayRoutineMaker";
 
 const DayRoutine = () => {
   const [hourText, setHourText] = useState([]);
+  const [chartReRenderHelper, setChartReRenderHelper] = useState([1]);
   const [addRoutineModalIsShown, setAddRoutineModalIsShown] = useState(false);
 
+  const myRountineSeries = useSelector((state) => state.job.series);
+  const myRountineStartAngle = useSelector((state) => state.job.angleRange);
+  const data = getSeriesData(myRountineSeries);
+  const options = getRoutineTableOptions(myRountineStartAngle);
+
+  const chartRef = useRef(null);
+
   useEffect(() => {
+    // 시간 텍스트 세팅
     const numArr = Array.from({ length: 24 }, (v, i) => i + 1);
+    numArr[23] = 0;
     setHourText(numArr);
+
+    // API 호출 (해당 날짜 전체 일과)
   }, []);
 
   const showAddRountineHandler = () => {
@@ -93,18 +35,32 @@ const DayRoutine = () => {
   };
   const hideAddRountineHandler = () => {
     setAddRoutineModalIsShown(false);
+
+    setChartReRenderHelper([...chartReRenderHelper, 1]);
+    chartRef.current.getInstance().destroy();
+    document
+      .querySelector(".day-routine-wrapper")
+      .removeChild(document.querySelector(".day-routine-wrapper > div"));
   };
 
   return (
     <>
-      {addRoutineModalIsShown && <Modal onClose={hideAddRountineHandler}><DayRoutineMaker /></Modal>}
+      {addRoutineModalIsShown && (
+        <Modal onClose={hideAddRountineHandler}>
+          <DayRoutineMaker onClose={hideAddRountineHandler} />
+        </Modal>
+      )}
       <h1>MY ROUTINE</h1>
       <div className="day-routine-container">
         <div className="add-btn-wrapper">
-          <button className="add-btn-job" onClick={showAddRountineHandler}>일과 추가</button>
+          <button className="add-btn-job" onClick={showAddRountineHandler}>
+            일과 추가
+          </button>
         </div>
         <div className="day-routine-wrapper">
-          <PieChart data={data} options={options} />
+          {chartReRenderHelper.map((t, i) => (
+            <PieChart ref={chartRef} data={data} options={options} key={i} />
+          ))}
           {hourText.map((hour) => (
             <p key={hour}>{hour}</p>
           ))}
---
+++ src/store/job.js
@@ -0,0 +1,82 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+import moment from "moment";
+
+const jobToDayRoutine = (state, action) => {
+  const { content, startTime } = action.payload;
+
+  let jobs = [...state.jobs, [content, Number(startTime)]];
+  jobs = jobs.sort((a, b) => {
+    return a[1] - b[1];
+  });
+
+  const series = [];
+  let totalTime = 0;
+  if (jobs.length > 1) {
+    for (let i = 0; i < jobs.length - 1; i++) {
+      let timeGap = jobs[i + 1][1] - jobs[i][1];
+      let periodTime = Math.floor(timeGap / 100) + (timeGap % 100 > 0 ? 0.5 : 0)
+      
+      series.push({ name: jobs[i][0], data: periodTime});
+      totalTime += periodTime;
+    }
+    series.push({ name: jobs[jobs.length - 1][0], data: 24 - totalTime });
+  } else {
+    series.push({ name: jobs[0][0], data: jobs[0][1] });
+  }
+
+  state.jobs = jobs;
+  return series;
+};
+
+const getStartEndAngle = (state) => {
+  const startTime = state.jobs[0][1];
+  const startAngle =  Math.floor(startTime / 100) + (startTime % 100 > 0 ? 0.5 : 0)
+  
+  return {
+    start: 15*startAngle,
+    end: 360+(15*startAngle),
+  };
+};
+
+const initialJobState = {
+  currItem: {
+    id: 0,
+    data: {
+      content: "",
+      isFinish: false,
+      startTime: moment().format("HH:mm"),
+      endTime: moment().format("HH:mm"),
+    },
+  },
+  series: [],
+  jobs: [],
+  angleRange: { start: 0, end: 360 },
+  date: moment().format("dddd, Do MMMM"),
+};
+
+const jobSlice = createSlice({
+  name: "job",
+  initialState: initialJobState,
+  reducers: {
+    addJob(state, action) {
+      state.series = jobToDayRoutine(state, action);
+      state.angleRange = getStartEndAngle(state);
+    },
+    setContent(state, action) {
+      state.currItem.data.content = action.payload;
+    },
+    setStartTime(state, action) {
+      state.currItem.data.startTime = action.payload;
+    },
+    setEndTime(state, action) {
+      state.currItem.data.endTime = action.payload;
+    },
+    setIsFinished(state, action) {
+      state.currItem.data.isFinish = action.payload;
+    },
+  },
+});
+
+export const jobActions = jobSlice.actions;
+export default jobSlice.reducer;
