669fd1679a2322e905a855cffcf66b5fa46647b0
jungjaechoi <joong8812@gmail.com>
2022-05-18 00:30:20 +0900

    Feat: 추가 - 로그인 여부 state 추가 & 헤더 메뉴 추가 #4

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import jobReducer from "./job";
+import authReducer from "./auth";
 
 const store = configureStore({
-  reducer: { job: jobReducer,},
+reducer: { job: jobReducer, auth: authReducer},
 });
 
 export default store;
