556cb4a80b28bb100a7661ce271eeed166e27ccc
jungjaechoi <joong8812@gmail.com>
2022-05-20 23:44:43 +0900

    Feat: 추가 - 일정 업데이트 기능 #16

--- src/lib/util.js
+++ src/lib/util.js
@@ -1,3 +1,6 @@
+import moment from "moment";
+import * as CONSTANT from '../lib/constants'
+
 export const getHHmmFormat = (time) => {
   return `${time.slice(0, 2)}:${time.slice(-2)}`;
 };
@@ -7,22 +10,34 @@ export const removeElementFromArray = (arr, index) => {
   return arr;
 };
 
+export const updateElementFromArray = (arr, selectedJob) => {
+  arr[selectedJob.seriesId][CONSTANT.INDEX_OF_CONTENT] = selectedJob.data.content;
+  arr[selectedJob.seriesId][CONSTANT.INDEX_OF_STARTTIME] = Number(moment(selectedJob.data.start_time).format("HHmm"));
+  arr[selectedJob.seriesId][CONSTANT.INDEX_OF_IS_FINISH] = selectedJob.data.is_finished;
+
+  return arr;
+};
+
 export const jobToDayRoutine = (jobs) => {
   const series = [];
   let totalTime = 0;
   
+  jobs = jobs.sort((a, b) => {
+    return a[CONSTANT.INDEX_OF_STARTTIME] - b[CONSTANT.INDEX_OF_STARTTIME];
+  }
+  );
   if (jobs.length !== 0) {
     if (jobs.length > 1) {
       for (let i = 0; i < jobs.length - 1; i++) {
-        let timeGap = jobs[i + 1][1] - jobs[i][1];
+        let timeGap = jobs[i + 1][CONSTANT.INDEX_OF_STARTTIME] - jobs[i][CONSTANT.INDEX_OF_STARTTIME];
         let periodTime = Math.floor(timeGap / 100) + (timeGap % 100 > 0 ? 0.5 : 0);
 
-        series.push({ name: jobs[i][0], data: periodTime });
+        series.push({ name: jobs[i][CONSTANT.INDEX_OF_CONTENT], data: periodTime });
         totalTime += periodTime;
       }
-      series.push({ name: jobs[jobs.length - 1][0], data: 24 - totalTime });
+      series.push({ name: jobs[jobs.length - 1][CONSTANT.INDEX_OF_CONTENT], data: 24 - totalTime });
     } else {
-      series.push({ name: jobs[0][0], data: jobs[0][1] });
+      series.push({ name: jobs[0][CONSTANT.INDEX_OF_CONTENT], data: jobs[0][CONSTANT.INDEX_OF_STARTTIME] });
     }
   }
   return series;
