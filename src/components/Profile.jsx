838071e5a51b36cb8239d2ae7a4f81e4f69860fd
kkitokki <kyaxxt@gmail.com>
2022-05-23 18:13:26 +0900

    fix : 수정 - mypage

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
-
+import '../Mypage.css';
+import {useSelector} from "react-redux";
 
 
 function Profile() {
+
+  const user = useSelector((state) => state.auth.user);
   return (
     <div className="profile">
+      <img id="profile" src="../assets/profile.png" />
+      <h5>안녕하세요. <span>{user.nickname}</span>님</h5>
+      <h5><span>mail : </span>{user.email}</h5>
+      <button>회원정보수정</button>
 
+        <h3>my routine wave</h3>
+        <img src="../assets/achievement.png" />
+        <h3>my radar</h3>
+        <img src="../assets/radar.png" />
     </div>
   );
 }
