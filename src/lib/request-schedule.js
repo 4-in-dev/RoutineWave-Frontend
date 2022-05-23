e273535e5cfdacd828bf74ffeff45b9529bceab9
KIMHOONHEE <nmdkims@gmail.com>
2022-05-23 10:00:44 +0900

    Hotfix:수정 - request-schedule.js serverUrl 테스트 #21

--- src/lib/request-schedule.js
+++ src/lib/request-schedule.js
@@ -1,62 +1,63 @@
 export const reqDeleteJob = async (jobId, cookies) => {
-  // const serverUrl = `/api/schedule/${jobId}/`;
-  // eslint-disable-next-line no-template-curly-in-string
-  const serverUrl = process.env.REACT_APP_SERVER_URL + "api/schedule/${jobId}/";
-  const reqData = {
-    method: "DELETE",
-    headers: {
-      "Content-Type": "application/json",
-      "Authorization": `Bearer ${cookies.is_login}`,
-      "Access-Control-Allow-Origin": '*',
-      "X-CSRFToken": cookies.csrftoken
-    },
-  };
-  try {
-    await fetch(serverUrl, reqData);
-    return true;
-  } catch (error) {
-    console.log(error.message);
-    return false;
-  }
+    // const serverUrl = `/api/schedule/${jobId}/`;
+    // eslint-disable-next-line no-template-curly-in-string
+    const serverUrl = process.env.REACT_APP_SERVER_URL + "api/schedule/${jobId}/";
+    const reqData = {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${cookies.is_login}`,
+            "Access-Control-Allow-Origin": '*',
+            "X-CSRFToken": cookies.csrftoken
+        },
+    };
+    try {
+        await fetch(serverUrl, reqData);
+        return true;
+    } catch (error) {
+        console.log(error.message);
+        return false;
+    }
 };
 
 
 export const reqDayJob = async (targetDay, accessToken) => {
-  const serverUrl = `/api/schedule?day=${targetDay}`;
-  const reqData = {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      "Authorization": `Bearer ${accessToken}`,
-      "Access-Control-Allow-Origin": '*'
-    },
-  };
-  try {
-    const response = await fetch(serverUrl, reqData);
-    const responseData = await response.json();
-    return responseData;
-  } catch (error) {
-    console.log(error.message);
-    return null;
-  }
+    // eslint-disable-next-line no-template-curly-in-string
+    const serverUrl = process.env.REACT_APP_SERVER_URL + `api/schedule?day=${targetDay}`;
+    const reqData = {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${accessToken}`,
+            "Access-Control-Allow-Origin": '*'
+        },
+    };
+    try {
+        const response = await fetch(serverUrl, reqData);
+        const responseData = await response.json();
+        return responseData;
+    } catch (error) {
+        console.log(error.message);
+        return null;
+    }
 };
 
 export const reqMonthJob = async (targetMonth, accessToken) => {
-  const serverUrl = `/api/schedule?month=${targetMonth}`;
-  const reqData = {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      "Authorization": `Bearer ${accessToken}`,
-      "Access-Control-Allow-Origin": '*'
-    },
-  };
-  try {
-    const response = await fetch(serverUrl, reqData);
-    const responseData = await response.json();
-    return responseData;
-  } catch (error) {
-    console.log(error.message);
-    return null;
-  }
+    const serverUrl = process.env.REACT_APP_SERVER_URL + `/api/schedule?month=${targetMonth}`;
+    const reqData = {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${accessToken}`,
+            "Access-Control-Allow-Origin": '*'
+        },
+    };
+    try {
+        const response = await fetch(serverUrl, reqData);
+        const responseData = await response.json();
+        return responseData;
+    } catch (error) {
+        console.log(error.message);
+        return null;
+    }
 };
