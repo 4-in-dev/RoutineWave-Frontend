4c75d385e60d05e6224e935624e5523e6b0204db
jungjaechoi <joong8812@gmail.com>
2022-05-23 15:12:48 +0900

    Design:  수정 - nav바 변경에 따른 페이지 css 수정

--- src/pages/User/SignupPage.js
+++ src/pages/User/SignupPage.js
@@ -1,11 +1,19 @@
 import React from "react";
 
 import Signup from '../../components/Signup/Signup'
+import styled from 'styled-components' 
 
 const SignupPage = () => {
   return (
-    <Signup />
+    <SignupWrapper>
+      <Signup />
+    </SignupWrapper>
   );
 };
+
+const SignupWrapper = styled.div`
+  margin: 119px auto 0 auto;
+  max-width: 980px;
+`
 
 export default SignupPage;
